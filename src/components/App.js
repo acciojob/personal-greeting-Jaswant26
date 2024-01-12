0fa286c36c92678f25f0ecf12de87e9c792062bd
Jaswant Singh Rawat <71880220+Jaswant26@users.noreply.github.com>
2024-01-12 17:07:50 +0000

    final push

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,23 @@
 
-import React from "react";
+
+import React,{useState} from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const[text,setText]=useState("")
+
+  function getValue(e){
+    setText(e.target.value);
+  }
   return (
     <div>
-        {/* Do not remove the main div */}
+      <label>Enter your name:</label>
+      <br></br>
+        <input type="text" placeholder="" onChange={getValue}></input>
+        <p>{text}</p>
     </div>
   )
 }
 
 export default App
+
